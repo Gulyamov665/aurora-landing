59044ed16beacb9424015c1ab469ce7c68ec109f
Siroch <sirochtv@gmail.com>
2025-06-12 17:49:05 +0500

    Add benefits section and delivery description component to enhance delivery features

---
+++ src/apps/features/delivery/components/DeliveryDescriptionBox.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Box, Typography, Container, Grid, List, ListItem, ListItemText } from '@mui/material'
+// Замени путь на актуальный для себя
+
+interface Feature {
+    title: string;
+    description?: string;
+    items?: string[];
+    img: string
+}
+
+interface DeliveryDescriptionItemProps {
+    item: Feature;
+    reverse?: boolean;
+
+}
+
+export const DeliveryDescriptionItem: React.FC<DeliveryDescriptionItemProps> = ({ item, reverse }) => {
+    return (
+        <Box sx={{ py: { xs: 6, md: 12 } }}>
+            <Container maxWidth="lg">
+                <Typography
+                    variant="h4"
+                    fontWeight="bold"
+                    textAlign="center"
+                    mb={8}
+                >
+                    Возможности сайта для приема заказов
+                </Typography>
+
+                <Grid
+                    container
+                    spacing={4}
+                    alignItems="center"
+                    justifyContent="center"
+                    direction={reverse ? 'row-reverse' : 'row'}
+                >
+                    {/* Картинка телефона */}
+                    <Grid item xs={12} md={6}>
+                        <Box
+                            component="img"
+                            src={item.img}
+                            alt="Меню на телефоне"
+                            sx={{ width: '50%', maxWidth: 400, mx: 'auto', display: 'block' }}
+                        />
+                    </Grid>
+
+                    {/* Текст справа */}
+                    <Grid item xs={12} md={6}>
+                        <Typography variant="h5" fontWeight="medium" gutterBottom>
+                            {item.title}
+                        </Typography>
+                        <List >
+                            {item.items?.map((text, index) => (
+                                <ListItem disableGutters key={index}>
+                                    <ListItemText primary={text} />
+                                </ListItem>
+                            ))}
+
+                        </List>
+                    </Grid>
+                </Grid>
+            </Container>
+        </Box>
+    )
+}
--- src/apps/features/delivery/pages/Delivery.tsx
+++ src/apps/features/delivery/pages/Delivery.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 import { Advantages } from '../../advantages/Advantages'
-import { deliveryFeatures } from '@/apps/content/advantagesContent'
+import { benefits, deliveryFeatures } from '@/apps/content/advantagesContent'
 import { DeliverHero } from '../components/DeliverHero'
 import { Container } from '@mui/material'
+import { DeliveryDescriptionItem } from '../components/DeliveryDescriptionBox'
 
 export const Delivery: React.FC = () => {
   return (
@@ -11,6 +12,9 @@ export const Delivery: React.FC = () => {
       <DeliverHero />
       <Container maxWidth="lg" sx={{ py: 8 }}>
         <Advantages features={deliveryFeatures} />
+        {benefits.map((item, index) => (
+          <DeliveryDescriptionItem item={item} key={index} reverse={index % 2 === 0} />
+               ))}
       </Container>
     </>
   )
